e4c7c685762f2db86d50ef40cbf25d48e51489a1
Joacim Eberlen <jeberlen@gmail.com>
2020-08-23 11:02:10 +0200

    Issue-#4: Application checks for camera permission and ask user to grant it

--- app/src/main/java/com/jeberlen/qrunner/MainActivity.kt
+++ app/src/main/java/com/jeberlen/qrunner/MainActivity.kt
@@ -1,24 +1,23 @@
 package com.jeberlen.qrunner
 
+import android.Manifest
 import android.content.Intent
-import android.graphics.BitmapFactory
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.provider.AlarmClock
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.vision.Frame
-import com.jeberlen.qrunner.scanner.QScanner
+import androidx.core.content.ContextCompat
 import com.jeberlen.qrunner.scanner.QScannerActivity
-import com.jeberlen.qrunner.viewer.AppViewerActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val intent = Intent(this, QScannerActivity::class.java)
-        startActivity(intent)
+        permissionCheck()
 
         /*
         // Keep this for later use - might be moved to a test class
@@ -37,7 +36,38 @@ class MainActivity : AppCompatActivity() {
 
         startActivity(intent)
         */
-
     }
 
+    private fun permissionCheck() {
+        val permission = Manifest.permission.CAMERA
+        when {
+            ContextCompat.checkSelfPermission(this, permission) ==
+                PackageManager.PERMISSION_GRANTED -> {
+                    val intent = Intent(
+                        this,
+                        QScannerActivity::class.java
+                    )
+                    startActivity(intent)
+                }
+                shouldShowRequestPermissionRationale(permission) -> {
+                    requestPermissionLauncher.launch(permission)
+                } else -> {
+                    requestPermissionLauncher.launch(permission)
+                }
+        }
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                val intent = Intent(this, QScannerActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this,
+                    "This application needs camera permission " +
+                            "to function, please restart the app",
+                    Toast.LENGTH_LONG).show()
+            }
+        }
 }
